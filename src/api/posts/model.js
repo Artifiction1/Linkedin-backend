22b0180d2af0cee7b18a592fc2fb2daf6ea54532
marios <mgeorgak859@gmail.com>
2022-08-22 17:45:46 +0300

    Update model.js

--- src/api/posts/model.js
+++ src/api/posts/model.js
@@ -1,3 +1,10 @@
 import mongoose, { mongo } from "mongoose"
 
 const { Schema, model} = mongoose
+
+const postsSchema = new Schema(
+    {
+        text: {type:String, required:true},
+        username: {}
+    }
+)
