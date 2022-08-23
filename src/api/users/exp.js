bfe30f06a9b646c96ca93d28985c8af01cc80f56
marios <mgeorgak859@gmail.com>
2022-08-23 16:08:45 +0300

    updated exp Schema

--- src/api/users/exp.js
+++ src/api/users/exp.js
@@ -10,9 +10,11 @@ const experiencesSchema = new Schema(
     {
         role: {type: String, required: true},
         company: {type: String, required: true},
+        startDate:{type: Date, required:true},
+        endDate:{type:Date},
         description: {type:String, required: true},
         area: {type:String, required:true},
-        
+        image:{type:String, required:true}
     },
     {
         timestamps: true,
