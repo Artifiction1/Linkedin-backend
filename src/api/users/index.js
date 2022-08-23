c300babcf05f9ce6cb39d8f9413f84e1cb0cfe78
marios <mgeorgak859@gmail.com>
2022-08-23 14:22:06 +0300

    updated users api

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import UsersModel from "./model.js";
 import ExperiencesModel from "./exp.js";
-import createHttpError from "http-errors"
+import createHttpError from "http-errors";
 const usersRouter = express.Router();
 
 usersRouter.post("/", async (req, res, next) => {
@@ -16,7 +16,7 @@ usersRouter.post("/", async (req, res, next) => {
 
 usersRouter.get("/", async (req, res, next) => {
   try {
-    const users = await UsersModel.find().populate({path:"experiences"});
+    const users = await UsersModel.find().populate({ path: "experiences" });
     res.send(users);
   } catch (error) {
     next(error);
@@ -25,7 +25,9 @@ usersRouter.get("/", async (req, res, next) => {
 
 usersRouter.get("/:userId", async (req, res, next) => {
   try {
-    const user = await UsersModel.findById(req.params.userId).populate({path:"experiences"});
+    const user = await UsersModel.findById(req.params.userId).populate({
+      path: "experiences",
+    });
     if (user) {
       res.send(user);
     } else {
@@ -41,15 +43,17 @@ usersRouter.get("/:userId", async (req, res, next) => {
 usersRouter.put("/:userId", async (req, res, next) => {
   try {
     const updatedUser = await UsersModel.findByIdAndUpdate(
-        req.params.userId, 
-        req.body, 
-        { new: true, runValidators: true } 
-      )
-      if (updatedUser) {
-        res.send(updatedUser)
-      } else {
-        next(createHttpError(404, `User with id ${req.params.userId} not found!`))
-      }
+      req.params.userId,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (updatedUser) {
+      res.send(updatedUser);
+    } else {
+      next(
+        createHttpError(404, `User with id ${req.params.userId} not found!`)
+      );
+    }
   } catch (error) {
     next(error);
   }
@@ -57,43 +61,107 @@ usersRouter.put("/:userId", async (req, res, next) => {
 
 usersRouter.delete("/:userId", async (req, res, next) => {
   try {
-    const deletedUser = await UsersModel.findByIdAndDelete(req.params.userId)
+    const deletedUser = await UsersModel.findByIdAndDelete(req.params.userId);
     if (deletedUser) {
-      res.status(204).send()
+      res.status(204).send();
     } else {
-      next(createHttpError(404, `User with id ${req.params.userId} not found!`))
+      next(
+        createHttpError(404, `User with id ${req.params.userId} not found!`)
+      );
     }
   } catch (error) {
     next(error);
   }
 });
 
-usersRouter.post("/:userId/experiences", async (req,res,next)=>{
-    try {
-        const newExperience = new ExperiencesModel(req.body);
-        const { _id } = await newExperience.save();
-        res.status(201).send({ _id });
-    
-  
-    } catch (error) {
-      next(error)
-    }
-  })
+usersRouter.post("/:userId/experiences", async (req, res, next) => {
+  try {
+    const newExperience = new ExperiencesModel(req.body);
+    const { _id } = await newExperience.save();
+    const user = await UsersModel.findById(req.params.userId);
+    user.experiences.push(_id);
+    await user.save();
+    res.status(201).send({ _id });
+  } catch (error) {
+    next(error);
+  }
+});
 
-  usersRouter.get("/all/experiences", async (req, res, next) => {
+usersRouter.get("/:userId/experiences", async (req, res, next) => {
+  try {
+    const user = await UsersModel.findById(req.params.userId).populate({
+      path: "experiences",
+    });
+    if (user) {
+      res.send(user.experiences);
+    } else {
+      next(
+        createHttpError(404, `User with id ${req.params.userId} not found!`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.get(
+  "/:usersId/experiences/:experienceId",
+  async (req, res, next) => {
     try {
-      const experiences = await ExperiencesModel.find();
-      res.send(experiences);
+      const user = await UsersModel.findById(req.params.usersId).populate({
+        path: "experiences",
+      });
+      if (user) {
+        const experience = user.experiences.find(
+          (current) => req.params.experienceId === current._id.toString()
+        );
+        if (experience) {
+          res.send(experience);
+        } else {
+          next(
+            createHttpError(
+              404,
+              `Experience with id ${req.params.experienceId} not found!`
+            )
+          );
+        }
+      } else {
+        next(
+          createHttpError(404, `User with id ${req.params.usersId} not found!`)
+        );
+      }
     } catch (error) {
       next(error);
     }
-  });
-  
-  usersRouter.get("/:userId/experiences", async (req, res, next) => {
+  }
+);
+
+usersRouter.put(
+  "/:userId/experiences/:experienceId",
+  async (req, res, next) => {
     try {
-      const user = await UsersModel.findById(req.params.userId).populate({path:"experiences"});
+      const user = await UsersModel.findById(req.params.userId).populate({
+        path: "experiences",
+      });
+
       if (user) {
-        res.send(user.experiences);
+        const experience = await ExperiencesModel.findByIdAndUpdate(
+          req.params.experienceId,
+          req.body,
+          { new: true, runValidators: true }
+        );
+
+        if (experience) {
+          await user.save();
+          res.send(experience);
+        } else {
+          next(
+            createHttpError(
+              404,
+              `Experience with id ${req.params.experienceId} not found!`
+            )
+          );
+        }
       } else {
         next(
           createHttpError(404, `User with id ${req.params.userId} not found!`)
@@ -102,6 +170,38 @@ usersRouter.post("/:userId/experiences", async (req,res,next)=>{
     } catch (error) {
       next(error);
     }
+  }
+);
+
+usersRouter.delete("/:userId/experiences/:experienceId", async (req, res, next)=>{
+try {
+  const user = await UsersModel.findById(req.params.userId).populate({
+    path: "experiences",
   });
+
+  if (user) {
+    const experience = await ExperiencesModel.findByIdAndDelete( req.params.experienceId );
+
+    if (experience) {
+      await user.save();
+      res.send();
+    } else {
+      next(
+        createHttpError(
+          404,
+          `Experience with id ${req.params.experienceId} not found!`
+        )
+      );
+    }
+  } else {
+    next(
+      createHttpError(404, `User with id ${req.params.userId} not found!`)
+    );
+  }
+} catch (error) {
+  next(error);
+}
+})
+ 
 
 export default usersRouter;
